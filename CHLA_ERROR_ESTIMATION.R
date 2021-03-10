892346c02551e11440555ea08bf631f8133bdb99
dmqc <schmechtig@obs-vlfr.fr>
2021-03-10 18:16:48 +0100

    Correct the calculation of CHLA_ERROR_ESTIMATION

--- CHLA_ERROR_ESTIMATION.R
+++ CHLA_ERROR_ESTIMATION.R
@@ -1,12 +1,14 @@
-CHLA_ERROR_ESTIMATION <- function (filenc,CHLA_ADJUSTED,ERROR,index_param) {
+CHLA_ERROR_ESTIMATION <- function (filenc,CHLA_ADJUSTED,ERROR) {
 
 PRES=ncvar_get(filenc,"PRES")
 
 CHLA_ADJUSTED_ERROR=ncvar_get(filenc,"CHLA_ADJUSTED_ERROR")
 
-i_prof_param=index_param[,3]
+ERROR_TEMP=abs(ERROR*CHLA_ADJUSTED)
 
-CHLA_ADJUSTED_ERROR[,i_prof_param]=max(0.02,abs(ERROR*CHLA_ADJUSTED))
+ERROR_TEMP[which(ERROR_TEMP<0.02)]=0.02
+
+CHLA_ADJUSTED_ERROR=ERROR_TEMP
 
 return(CHLA_ADJUSTED_ERROR)
 
